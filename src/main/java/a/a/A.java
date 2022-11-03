011e59d579552768bd3e4017af554b88c1ebde06
yuta <y82396267@gmail.com>
2022-11-04 00:25:57 +0900

    Initial commit

---
+++ src/main/java/a/a/A.java
@@ -0,0 +1,17 @@
+package a.a;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class A extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
